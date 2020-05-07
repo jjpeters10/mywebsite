f210968abd4f791c34b9e10bdfedf6cecdaff82d
Julian <jjpeters10@gmail.com>
2020-05-07 17:31:16 -0400

    Changing content and adding pdf display

--- website.py
+++ website.py
@@ -1,11 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-
     return render_template("index.html")
+
+
+@app.route('/Resume')
+def show_resume():
+    return send_file('static/images/Resume_Hardware.pdf', attachment_filename='Resume.pdf')
+
+
+@app.route('/Portfolio')
+def show_portfolio():
+    return send_file('static/images/Project_Portfolio_Hardware.pdf', attachment_filename='Project_Portfolio.pdf')
 
 if __name__=='__main__':
     app.run(debug = True)
